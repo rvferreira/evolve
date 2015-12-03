93b01eba59c4a7b135e8a445feba92c5c575ccd9
rvferreira <raphael.rvf@gmail.com>
2015-12-03 16:16:42 -0200

    learning json has been read

--- web-server/src/CoP/urls.py
+++ web-server/src/CoP/urls.py
@@ -1,17 +1,17 @@
 """CoP URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+	https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+	1. Add an import:  from my_app import views
+	2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+	1. Add an import:  from other_app.views import Home
+	2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+	1. Add an import:  from blog import urls as blog_urls
+	2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -21,10 +21,10 @@ from evolutives.views import get_preloader
 from learning.views import index as learning_index
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^evaluate/', include('evolutives.urls')),
-    url(r'^learning/', learning_index, name="learning"),
-    url(r'^team/', TemplateView.as_view(template_name='team.html'), name="learning"),
-    url(r'^get_preloader$', get_preloader, name='get_preloader'),
-    url(r'^$', RedirectView.as_view(url='evaluate/')),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^evaluate/', include('evolutives.urls')),
+	url(r'^learning/', learning_index, name="learning"),
+	url(r'^team/', TemplateView.as_view(template_name='team.html'), name="learning"),
+	url(r'^get_preloader$', get_preloader, name='get_preloader'),
+	url(r'^$', RedirectView.as_view(url='evaluate/')),
 ]
--- web-server/src/learning/views.py
+++ web-server/src/learning/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from django.utils.safestring import SafeString
+import os
+from django.conf import settings
+
 # Create your views here.
 def index(request):
 
 	template = loader.get_template('learning/index.html')
+	content = open(os.path.join(settings.STATIC_ROOT, 'learning/learning.json'), 'rb').read()
+
 	context = RequestContext(request, {
 		'page_title': 'Wiki',
+		'content': SafeString(content),
 	})
 
 	return HttpResponse(template.render(context))
